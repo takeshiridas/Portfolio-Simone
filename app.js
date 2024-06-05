86020d3dbdeb2ddb39dd483c65d7a047302c9e5a
Takeshi <134627945+takeshiridas@users.noreply.github.com>
2024-06-05 13:58:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+    showSlides();
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
+function showSlides() {
     var i;
     var slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
 
